f8ac3030eb6f2b93ca028df7b11206f751c6732c
Pedro Carreira <pedro.henrique.cbl@gmail.com>
2022-03-30 19:59:42 +0100

    Add and Remove Category Test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,6 +1,7 @@
 /// <reference types="Cypress" />
 
-describe('Testes', () => {
+describe('FRUTOS OESTE TESTING', () => {
+    const filepath = 'images/biologicos.jpg'
     it('Login', () => {
         cy.visit('http://frutos.test/admin')
         cy.get('.login-form .div')
@@ -21,5 +22,33 @@ describe('Testes', () => {
         cy.get('.list-group-item')
             .last()
             .click()
+        
+        cy.get('.card-header .btn')
+            .click()
+
+        cy.get('.form-control')
+            
+            .type('Bio')
+            .get('input[type="file"]').attachFile(filepath)
+            .get('.form-group .btn')
+            .first()
+            .click()
+
+        cy.get('tbody')
+            .should('have.length', 7)
+    })
+
+    it('Remove Category', () => {
+        cy.get('table')
+        .contains('tbody td','Bio')
+        .next()
+        .next()
+        .then(elem => {
+            console.log(elem)
+            elem[0].children[0].firstChild.click()
+        })
+        cy.get('tbody')
+        .should('have.length', 6)
+        
     })
 })
