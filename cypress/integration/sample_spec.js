51a57b811db9a6391919492c71569b66bbe019bd
Pedro Carreira <pedro.henrique.cbl@gmail.com>
2022-03-30 14:25:49 +0100

    Login & get to Category List test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,7 +1,25 @@
 /// <reference types="Cypress" />
 
 describe('Testes', () => {
-    it('Visita site', () => {
-        cy.visit('http://frutos.test/')
+    it('Login', () => {
+        cy.visit('http://frutos.test/admin')
+        cy.get('.login-form .div')
+            .first()
+            .type('admin@email.pt')
+            .next()
+            .type('123456\r')            
+    })
+
+    it('Add Category', () => {
+        cy.get('.navbar-nav .nav-item')
+            .first()
+            .next()
+            .next()
+            .next()
+            .click()       
+        
+        cy.get('.list-group-item')
+            .last()
+            .click()
     })
 })
